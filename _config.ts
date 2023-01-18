ab8ba98f32436ddcfe3fd938afd431d3444ddf3c
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2023-01-18 12:05:06 +0700

    build: Replace esbuild with rollup+sucrase+esbuild

--- _config.ts
+++ _config.ts
@@ -1,10 +1,12 @@
-import modify_urls from "lume/plugins/modify_urls.ts";
 import babel from "drsensor/lume-plugins/babel.ts";
 import { type Options as BuildOptions } from "lume/plugins/esbuild.ts";
 import source_maps from "lume/plugins/source_maps.ts";
 import minify_html from "lume/plugins/minify_html.ts";
 
-import * as es from "esbuild";
+// import routes from "lume/middlewares/redirects.ts";
+import * as babel_importmap from "npm:babel-plugin-import-map";
+import modify_urls from "lume/plugins/modify_urls.ts";
+
 // TODO: try https://github.com/sanyuan0704/vite-plugin-chunk-split
 /* TODO: create rollup-plugin-swc-wasm based on:
 https://github.com/egoist/unplugin-swc
@@ -13,38 +15,90 @@ https://github.com/SukkaW/rollup-plugin-swc
   or
 make PR to rollup-plugin-swc3 to support wasm
 */
-import { type OutputOptions, rollup, type RollupOptions } from "rollup";
+import { type OutputOptions, rollup, type RollupOptions } from "rollup"; // BUG(bundle): try Parcel! Rollup heuristics doesn't look great. It doesn't inline all `const` in constant/colon.ts
 import sucrase from "rollup/plugin-sucrase";
 import esbuild from "rollup/plugin-esbuild";
-import { swc } from "rollup/plugin-swc";
-import terser from "rollup/plugin-terser";
+// import { swc } from "rollup/plugin-swc";
+// import terser from "rollup/plugin-terser";
 
-import { join } from "deno/path/mod.ts";
-import { copy, emptyDir, type WalkEntry } from "deno/fs/mod.ts";
+import { basename, extname, isGlob, join, relative } from "deno/path/mod.ts";
+import { copy, emptyDir, expandGlob, walk } from "deno/fs/mod.ts";
 import lume from "lume/mod.ts";
 import Server from "lume/core/server.ts";
 
+const singleStarFiles = {
+  globstar: false,
+  extended: false,
+  includeDirs: false,
+} satisfies Parameters<typeof expandGlob>[1];
+
+let isMangle;
 const keep_whitespace = {
   minify: false,
   minifySyntax: true,
-  minifyIdentifiers: !Deno.env.get("NO_MANGLE"),
+  minifyIdentifiers: isMangle = !Deno.env.get("NO_MANGLE"),
   minifyWhitespace: false,
 } satisfies BuildOptions["options"];
 
+import type { Queue } from "./core/query.ts";
 const conf = {
   ...Deno.env.get("KEEP_WHITESPACE") || !keep_whitespace.minifyIdentifiers
     ? keep_whitespace
-    : { minify: true },
-  mangleProps: /^_/,
+    : { minify: isMangle = true },
   keepNames: false,
+  mangleProps: isMangle ? /[^_]_$/ : undefined,
+  mangleCache: {
+    attrs_: "a",
+    flags_: "f",
+    module_: "m",
+    scripts_: "j",
+    styles_: "c",
+    props_: "p",
+    events_: "e",
+  } satisfies Partial<
+    Record<
+      | keyof Queue
+      | keyof Queue["module_"]
+      | keyof Queue["attrs_"],
+      string
+    >
+  >,
 } satisfies BuildOptions["options"];
 
-const CI = Deno.env.get("CI");
 const site = lume();
+
+import npm from "./package.json" assert { type: "json" };
+const redirects = await Object.entries(npm.exports).reduce(
+  async (prev, [from, as]) => {
+    const excludes = ["types", "sourcemap"];
+    let to;
+    for (const [kind, path] of Object.entries(as)) {
+      if (excludes.some((it) => kind === it)) continue;
+      to = join("/nusa", path);
+      break;
+    }
+    if (to == null) return prev;
+    from = join("nusa", from);
+    const final = await prev;
+    if (isGlob(from)) {
+      for await (const { name, path } of expandGlob(from, singleStarFiles)) {
+        final[relative(Deno.cwd(), path).slice(0, -extname(path).length)] = to
+          .replace("*", basename(name, extname(name)));
+      }
+    } else final[from] = to;
+    return final;
+  },
+  Promise.resolve({} as Record<string, string>),
+);
+// server.use(routes({ redirects })); //<-- not needed because babel_importmap and modify_urls will replace the url
+babel_importmap.load([{ imports: redirects }]);
+site.use(modify_urls({
+  fn: (url) => url.startsWith("nusa") ? redirects[url] : url,
+}));
 
 site.script("test", async () => {
   await site.build();
-  const server = new Server({ port: 3000, root: site.dest() });
+  const server = new Server({ port: 3000, root: "_site" });
   server.start();
   const exit = await Deno.run({ cmd: ["deno", "task", "test"] }).status();
   server.stop();
@@ -52,16 +106,14 @@ site.script("test", async () => {
 });
 
 export default site
+  .loadAssets([".svg"])
   .ignore("core", "nusa", "demo/tester.ts", (path) => path.endsWith("test.ts"))
   .scopedUpdates((path) => path.endsWith(".ts") && !path.startsWith("_"))
-  .use(modify_urls({ fn: (url) => url.startsWith("nusa") ? `/${url}` : url }))
-  .use(babel())
-  // .use(esbuild({ extensions: [".mts"], options: { ...conf, splitting: true } }))
-  // .use(esbuild({ extensions: [".ts"], options: conf }))
+  .use(babel({ plugins: [babel_importmap.plugin()] }))
   .use(minify_html())
   .use(source_maps())
-  .use((site) => { // BUG(lume): currently they skip over symlinks https://github.com/lumeland/lume/blob/master/core/source.ts#L334-L336
-    const config: Parameters<typeof es.build>[0] = {
+  .use((site) => {
+    const config: BuildOptions["options"] = {
       outdir: join(site.options.dest, "nusa"),
       format: "esm",
       target: "esnext",
@@ -76,39 +128,37 @@ export default site
       config.sourcemap = "linked";
       site.run("bundle");
     });
-    site.addEventListener("beforeBuild", () => void (justBundling = false));
-    site.addEventListener("beforeBuild", "bundle");
-    if (!CI) site.addEventListener("beforeSave", () => done?.then(es.stop));
+    site.addEventListener("beforeBuild", () => {
+      justBundling = false;
+      config.sourcemap = "linked";
+      site.run("bundle");
+    });
 
-    let done: Promise<unknown> | undefined, justBundling = true;
+    let justBundling = true;
     site.script("bundle", async () => {
       const scripts: string[] = [], modules: string[] = [];
-      // BUG(deno): `walk` and `expandsGlob` must not skip over symlinks
-      // for await (const entry of walk("nusa", { includeDirs: false, exts: [".ts", ".mts"] })) {
-      //                           OR
-      // for await (const entry of expandGlob("nusa/**/*.{,m}ts")) {
-      //
-      for await (const entry of allFilesIn("nusa")) {
+
+      for await (const entry of walk("nusa", { includeDirs: false })) {
         if (entry.name.endsWith(".ts")) scripts.push(entry.path);
         else if (entry.name.endsWith(".mts")) modules.push(entry.path);
       }
       await emptyDir(config.outdir!);
 
-      if (CI) {
-        const cfg = {
-          input: {
-            treeshake: {
-              moduleSideEffects: false,
-              unknownGlobalSideEffects: false,
-              tryCatchDeoptimization: false,
-              propertyReadSideEffects: false,
-            },
-            plugins: [
-              sucrase({ // TODO: try SWC 🤔
-                transforms: ["typescript"],
-                disableESTransforms: true,
-              }),
-              /* BUG(deno): something wrong when using node:worker_threads
+      const whitespace = config.minify || config.minifyWhitespace ? "" : "\n";
+      const cfg = {
+        input: {
+          treeshake: {
+            moduleSideEffects: false,
+            unknownGlobalSideEffects: false,
+            tryCatchDeoptimization: false,
+            propertyReadSideEffects: false,
+          },
+          plugins: [
+            sucrase({ // TODO: try SWC 🤔
+              transforms: ["typescript"],
+              disableESTransforms: true,
+            }),
+            /* BUG(deno): something wrong when using node:worker_threads
                   error: Uncaught (in worker "$DENO_STD_NODE_WORKER_THREAD") Top-level await promise never resolved
                     [{ threadId, workerData, environmentData }] = await once(
                                                                   ^
@@ -116,51 +166,75 @@ export default site
                   error: Uncaught Error: Unhandled error in child worker.
                       at Worker.#pollControl (deno:runtime/js/11_workers.js:155:21)
               */
-              // config.minify ? terser(),
-              esbuild({
-                ...conf,
-                target: "esnext",
-                // BUG(rollup-plugin-esbuild): can't handle import from *.mts
-                // include: /\.[mc]?[jt]sx?$/,
-                // loader: { ".mts": "ts", ".ts": "ts" },
-                // resolveExtensions: [".ts", ".mts"],
-              }),
-            ],
-          } satisfies RollupOptions,
-          output: {
-            dir: config.outdir,
-            sourcemap: !!config.sourcemap,
-            generatedCode: {
-              constBindings: true,
-              objectShorthand: true,
-            },
-            compact: config.minify,
-          } satisfies OutputOptions,
-        };
-        await Promise.all([
-          rollup({
-            ...cfg.input,
-            external: modules.map((path) =>
-              path.replace("nusa/", "./").replace(".mts", ".js")
-            ),
-            input: scripts,
-            preserveEntrySignatures: false,
-          }).then((build) => build.write({ ...cfg.output, format: "iife" })),
-          rollup({
-            ...cfg.input,
-            input: modules,
-            preserveEntrySignatures: "allow-extension",
-          }).then((build) => build.write({ ...cfg.output, format: "module" })),
-        ]);
-      } else {
-        done = Promise.all([
-          es.build({ ...config, entryPoints: scripts }),
-          es.build({ ...config, entryPoints: modules, splitting: true }),
-        ]);
+            // config.minify ? terser(),
+            esbuild({
+              ...conf,
+              target: "esnext",
+              // BUG(rollup-plugin-esbuild): can't handle import from *.mts
+              // include: /\.[mc]?[jt]sx?$/,
+              // loader: { ".mts": "ts", ".ts": "ts" },
+              // resolveExtensions: [".ts", ".mts"],
+            }),
+          ],
+        } satisfies RollupOptions,
+
+        output: {
+          dir: config.outdir,
+          sourcemap: !!config.sourcemap,
+          freeze: false,
+          generatedCode: {
+            constBindings: true,
+            objectShorthand: true,
+          },
+          compact: config.minify,
+          format: "es",
+          externalLiveBindings: false,
+          hoistTransitiveImports: false,
+        } satisfies OutputOptions,
+      };
+
+      const build = await rollup({
+        ...cfg.input,
+        input: modules.concat(scripts),
+        preserveEntrySignatures: "allow-extension",
+      });
+
+      await build.write(cfg.output);
+
+      for (let file of scripts) {
+        let content = await Deno.readTextFile(
+          file = join(
+            config.outdir!,
+            file.slice("nusa/".length).slice(0, -".ts".length) + ".js",
+          ),
+        );
+        content = "(function(){" + whitespace + content;
+        if (config.sourcemap) {
+          content = content.replace(
+            "\n//# sourceMappingURL",
+            whitespace + "})()" + "\n//# sourceMappingURL",
+          );
+        } else {
+          content = content.slice(0, content.at(-2) === ";" ? -2 : -1);
+          content += whitespace + "})()" + "\n";
+        }
+        Deno.writeTextFile(file, content);
+      }
+
+      {
+        const bug = join(config.outdir!, "current.js");
+        const content = await Deno.readTextFile(bug);
+        const hash = content.match(/import.*from".\/registry-(.*).js/)![1];
+        Deno.writeTextFile(
+          bug,
+          `export{event,value}from"./registry-${hash}.js";\n` +
+            (config.sourcemap
+              ? `//# sourceMappingURL=registry-${hash}.js.map\n`
+              : ""),
+        );
       }
 
       if (justBundling) {
-        await done?.then(es.stop);
         copy("package.json", join(config.outdir!, "package.json"));
         console.log("🔥 /nusa/package.json");
       }
@@ -172,23 +246,3 @@ export default site
       }
     });
   });
-
-/** this is just a workaround until https://github.com/denoland/deno_std/issues/1359 fixed */
-async function* allFilesIn(root: string): AsyncIterableIterator<WalkEntry> {
-  for await (const entry of Deno.readDir(root)) {
-    if (entry.isSymlink) {
-      const realPath = await Deno.realPath(join(root, entry.name));
-      if ((await Deno.lstat(realPath)).isDirectory) {
-        yield* allFilesIn(realPath);
-        continue;
-      }
-    }
-    if (entry.isDirectory) {
-      yield* allFilesIn(join(root, entry.name));
-      continue;
-    }
-    const result = { ...entry } as WalkEntry;
-    result.path = join(root, entry.name);
-    yield result;
-  }
-}
