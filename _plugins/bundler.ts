35c1ce0f59cb800623388900b0ed9cd64eaff948
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2023-03-05 08:31:50 +0700

    build: Speed up bundle process
    * Cache subsequent build
    
    * generate->patch->write instead of write->read->patch->write
      (use `rollupBundle.generate` instead of `rollupBundle.write`)

--- _plugins/bundler.ts
+++ _plugins/bundler.ts
@@ -1,5 +1,5 @@
 import type { Site } from "lume/core.ts";
-import { join } from "deno/path/mod.ts";
+import { basename, join } from "deno/path/mod.ts";
 import { copy, emptyDir, expandGlob } from "deno/fs/mod.ts";
 
 import { type Options as BuildOptions } from "lume/plugins/esbuild.ts";
@@ -29,7 +29,11 @@ export default (conf: BuildConfig, pkgPath: string) => (site: Site) => {
   config.bundle = config.minify = true;
   Object.assign(config, conf);
 
-  site.copy("package.json", "nusa/package.json")
+  let cache: RollupOptions["cache"] | undefined = false;
+  site.copy("package.json", join(pkgPath, "package.json"))
+    .addEventListener("afterStartServer", () => {
+      cache = undefined; // enable cache
+    })
     .addEventListener("beforeUpdate", () => (
       config.watch = config.incremental = true, void build()
     ))
@@ -58,7 +62,7 @@ export default (conf: BuildConfig, pkgPath: string) => (site: Site) => {
     }
   };
   const entries = new Map<string, "script" | "import">();
-  site.hooks.importmapEntries?.(entries, ["script", "import"]);
+  site.hooks.importmapEntries?.(entries, ["script", "import"]); // BUG: only works on `deno task build`, mean that `deno task ci:bundle` will fail
 
   site.script("bundle", async () => {
     const [scripts, modules] = [...entries.entries()].reduce(
@@ -67,6 +71,7 @@ export default (conf: BuildConfig, pkgPath: string) => (site: Site) => {
       ),
       [[], []] as [string[], string[]],
     );
+    const nonModules = new Set(scripts.map((path) => basename(path, ".ts")));
     await emptyDir(config.outdir!);
 
     const whitespace = config.minify || config.minifyWhitespace ? "" : "\n";
@@ -79,10 +84,12 @@ export default (conf: BuildConfig, pkgPath: string) => (site: Site) => {
           propertyReadSideEffects: false,
         },
         plugins: [
+          /* WARNING: enable sucrase only when there is *.mts since rollup-plugin-esbuild not process them (most likely a bug cuz in the past typescript doesn't acknowledge .mts)
           sucrase({ // TODO: try SWC 🤔
             transforms: ["typescript"],
             disableESTransforms: true,
           }),
+          */
           /* BUG(deno): something wrong when using node:worker_threads
                   error: Uncaught (in worker "$DENO_STD_NODE_WORKER_THREAD") Top-level await promise never resolved
                     [{ threadId, workerData, environmentData }] = await once(
@@ -92,6 +99,7 @@ export default (conf: BuildConfig, pkgPath: string) => (site: Site) => {
                       at Worker.#pollControl (deno:runtime/js/11_workers.js:155:21)
               */
           // config.minify ? terser(),
+          // TODO: emit *.d.ts based on package.json#exports.*.types using https://github.com/Swatinem/rollup-plugin-dts
           // @ts-ignore: npm:rollup-plugin-esbuild doesn't provide type declaration
           esbuild({
             ...conf,
@@ -121,54 +129,60 @@ export default (conf: BuildConfig, pkgPath: string) => (site: Site) => {
 
     const build = await rollup({
       ...cfg.input,
-      input: modules.concat(scripts),
+      cache,
+      input: modules.concat(scripts), // BUG: `scripts` might be empty because `hooks.importmapEntries` only executed at `deno task build`
       preserveEntrySignatures: "allow-extension",
     });
+    if (cache !== false) cache = build.cache;
 
-    await build.write(cfg.output);
-
-    for (let file of scripts) {
-      let content = await Deno.readTextFile(
-        file = join(
-          config.outdir!,
-          file.slice("nusa/".length).slice(0, -".ts".length) + ".js",
-        ),
-      );
-      content = '"use strict";' + "(function(){" + whitespace + content;
-      if (config.sourcemap) {
-        content = content.replace(
-          "\n//# sourceMappingURL",
-          whitespace + "})()" + "\n//# sourceMappingURL",
-        );
-      } else {
-        content = content.slice(0, content.at(-2) === ";" ? -2 : -1);
-        content += whitespace + "})()" + "\n";
+    const gen = await build.generate(cfg.output);
+    for (const output of gen.output) {
+      const outputPath = join(config.outdir!, output.fileName);
+      const bugs = ["current.js"] as const;
+      switch (output.type) {
+        case "chunk": {
+          let code = output.code;
+          if (nonModules.has(output.name)) {
+            code = '"use strict";' + "(function(){" + whitespace + code;
+            if (config.sourcemap) {
+              code = code.replace(
+                "\n//# sourceMappingURL",
+                whitespace + "})()" + "\n//# sourceMappingURL",
+              );
+            } else {
+              code = code.slice(0, code.at(-2) === ";" ? -2 : -1);
+              code += whitespace + "})()" + "\n";
+            }
+          }
+          if (output.fileName === bugs[0]) {
+            const hash = code.match(/import.*from".\/registry-(.*).js/)![1];
+            code = `export{event,value}from"./registry-${hash}.js";\n` +
+              (config.sourcemap
+                ? `//# sourceMappingURL=registry-${hash}.js.map\n`
+                : "");
+          }
+          Deno.writeTextFile(outputPath, code); // TODO: remove this and use site.processAll()
+          break;
+        }
+        case "asset": {
+          const asset = output.source;
+          // TODO: remove this and use site.processAll()
+          if (typeof asset === "string") Deno.writeTextFile(outputPath, asset);
+          else Deno.writeFile(outputPath, asset);
+          break;
+        }
       }
-      Deno.writeTextFile(file, content);
-    }
-
-    {
-      const bug = join(config.outdir!, "current.js");
-      const content = await Deno.readTextFile(bug);
-      const hash = content.match(/import.*from".\/registry-(.*).js/)![1];
-      Deno.writeTextFile(
-        bug,
-        `export{event,value}from"./registry-${hash}.js";\n` +
-          (config.sourcemap
-            ? `//# sourceMappingURL=registry-${hash}.js.map\n`
-            : ""),
-      );
     }
 
     if (justBundling) {
       copy("package.json", join(config.outdir!, "package.json"));
-      console.log("🔥 /nusa/package.json");
+      console.log(`🔥 /${pkgPath}/package.json`);
     }
-    console.log("🔥 /nusa/*.js");
-    console.log("🔥 /nusa/*/*.js");
+    console.log(`🔥 /${pkgPath}/*.js`);
+    console.log(`🔥 /${pkgPath}/*/*.js`);
     if (config.sourcemap) {
-      console.log("🔥 /nusa/*.js.map");
-      console.log("🔥 /nusa/*/*.js.map");
+      console.log(`🔥 /${pkgPath}/*.js.map`);
+      console.log(`🔥 /${pkgPath}/*/*.js.map`);
     }
   });
 };
