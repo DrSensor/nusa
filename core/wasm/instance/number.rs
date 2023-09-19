a3697ba036b5f92c12616f984f3198c00a1cabed
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2023-09-19 20:00:38 +0700

    feat(wasm): Expose cABI for allocateAUTO

--- core/wasm/instance/number.rs
+++ core/wasm/instance/number.rs
@@ -3,7 +3,7 @@ mod scope;
 mod types;
 
 use core::arch::wasm32::{memory_grow, memory_size};
-use types::{number::Type, Null, Number, PAGE};
+use types::{number::Type, Null, Number, C, PAGE};
 
 #[export_name = "num.allocate"]
 #[inline(never)]
@@ -33,6 +33,15 @@ unsafe fn array_of(ty: Type, len: u16, nullable: bool) -> Number {
 }
 
 #[export_name = "num.allocateAUTO"]
-unsafe fn array_new(ty: Type, nullable: bool) -> Number {
-    array_of(ty, scope::size(), nullable)
+unsafe fn array_new(ty: Type, nullable: bool) -> (Number, u16) {
+    let size = scope::size();
+    (array_of(ty, size, nullable), size)
 }
+
+#[cfg(any(target_pointer_width = "32", target_pointer_width = "16"))]
+#[export_name = "num.cABIallocateAUTO"]
+unsafe fn c_array_new(ty: Type, nullable: bool) -> C::Tuple<Number, u16> {
+    C::Tuple::from(array_new(ty, nullable))
+}
+
+C::primitive_Item!(u16);
