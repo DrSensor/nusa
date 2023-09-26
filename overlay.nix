67ca1cdc246adbb8934e4b4cb4bdd7000886acc8
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2023-09-26 17:29:34 +0700

    devenv: Remove knit from overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -2,19 +2,5 @@ _final: prev: {
   lld = prev.lld_16;
   llvm = prev.llvm_16;
 
-  knit = prev.buildGoModule rec {
-    pname = "knit";
-    version = "1.1.1";
-    src = prev.fetchFromGitHub {
-      owner = "zyedidia";
-      repo = "knit";
-      rev = "v${version}";
-      hash = "sha256-zxwEJnQZpOEJhV7jx2ClS3XmMfGBiq8AHR26TOIBJVw=";
-    };
-    subPackages = [ "cmd/knit" ];
-    ldflags = [ "-s -w -X github.com/zyedidia/knit/info.Version=${version}" ];
-    vendorHash = "sha256-+IZFydwchHIMIvYmIgZ0uJKjW4aVBFuj3SQk58I0z/g=";
-  };
-
   # TODO: override stdenv to use toybox instead of GNU coreutils
 }
